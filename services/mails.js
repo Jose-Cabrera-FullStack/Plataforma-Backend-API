6b607a942a9d50d11a4b16b8a7e633e38bc44e16
Cabrera Jose (Atencion a Usuarios) <mda_jcabrera@lnexternos.com.ar>
2020-03-13 03:16:26 -0300

    se debe arregar el Error: redirect_uri_mismatch de el oauth

--- services/mails.js
+++ services/mails.js
@@ -1,27 +1,28 @@
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
-module.exports = (formulario) => {
+module.exports = (form) => {
     var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'SummonersCaves@gmail.com', // Cambialo por tu email
-            pass: 'josebeaguido' // Cambialo por tu password
+            user: process.env.EMAIL, 
+            pass: process.env.PASSWORD 
         }
     });
 
     let userHost = transporter.options.auth.user
 
     const mailOptions = {
-        from: `<${formulario.nombre}>`,
+        from: `<${form.nombre}>`,
         to: userHost, // Cambia esta parte por el destinatario
-        subject: formulario.asunto,
+        subject: form.case,
         html: `
-            <h1><i>Nombre:</i> ${formulario.nombre}</h1> <br/>
-            <h2><i>Asunto:</i> ${formulario.asunto} </h2> <br/>
-            <h3><i>E-mail:</i> ${formulario.email} </h3> <br/>
-            <h3>Mensaje:</h3> <h4>${formulario.mensaje}</h4>
+            <h1><i>Nombre:</i> ${form.name}</h1> <br/>
+            <h2><i>Asunto:</i> ${form.case} </h2> <br/>
+            <h3><i>E-mail:</i> ${form.email} </h3> <br/>
+            <h3>Mensaje:</h3> <h4>${form.message}</h4>
             `,
-        text: formulario.mensaje
+        text: form.message
     };
     transporter.sendMail(mailOptions, function (err, info) {
         if (err)
